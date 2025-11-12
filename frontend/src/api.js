8f034e0baf9123f2044eed560f27ccb3ed0dcce4
sinan-prvt <mohamedsinan9400@gmail.com>
2025-11-12 20:25:57 +0530

    Updated-api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "http://13.204.186.114/api/",
   headers: {
     "Content-Type": "application/json",
   },
