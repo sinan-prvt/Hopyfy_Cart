e0b5d052c8c875d9f6b5f2db32b42e6235aa4cdc
sinan-prvt <mohamedsinan9400@gmail.com>
2025-11-12 12:06:54 +0530

    Updated-urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
-def home(request):
-    return JsonResponse({"message": "Welcome to HopyfyCart API!"})
-
 urlpatterns = [
-    path('', home),  # 👈 This line adds the root route
     path('admin/', admin.site.urls),
     path('api/', include('main.urls')),
 ]
